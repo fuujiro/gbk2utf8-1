0b383e8e1c2451f45219008aa768249d1c1d1e9f
sydnash <sydnash@users.noreply.github.com>
2016-05-23 18:18:47 +0800

    Add files via upload

---
+++ basic/dirtool.py
@@ -0,0 +1,48 @@
+import os
+import os.path
+import sys
+
+def removeHeadTailSep(odir):
+    odir = odir.strip('/')
+    odir = odir.strip('\\')
+    return odir
+
+def replaceOdirToNdir(inputdir, odir, ndir):
+    lenth = len(odir)
+    relativedir = inputdir[lenth:]
+    relativedir = removeHeadTailSep(relativedir)
+    dirlist = [ndir, relativedir]
+    retdir = os.sep.join(dirlist)
+    return retdir
+
+def getAllFileInRootDir(rootdir, filter):
+    allFiles = []
+    retdirs = []
+    for parent,dirnames,filenames in os.walk(rootdir):
+        for dirname in  dirnames:                    
+            #print("parent is:" + parent)
+            #print("dirname is:" + dirname)
+            retdirs.append(os.path.join(parent, dirname))
+            
+            pass
+        for filename in filenames:                  
+            #print "parent is:" + parent
+            #print "filename is:" + filename
+            #print "the full name of the file is:" + os.path.join(parent,filename) 
+            ext = os.path.splitext(filename)[1][1:]
+            #print(ext)
+            if (ext == filter):
+                allFiles.append(os.path.join(parent, filename))
+    return allFiles, retdirs
+
+if __name__ == "__main__":
+    rootdir = sys.argv[1]
+    fileter = sys.argv[2]
+    #outdir = outdir
+    #key = sys.argv[3]
+    #sign = sys.argv[4]
+    files, dirs = getAllFileInRootDir(rootdir, fileter)
+    #print(dirs)
+    
+    #for file in files:
+     #   os.system('xxtea.exe')
